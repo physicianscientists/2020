946e176aa7ec5746b0bbd45d2cf98947cb295cb3
tikenn <tikennprogrammer@gmail.com>
2017-04-09 16:52:41 -0500

    streamlined JS (adussaq)

--- bootstrap/js/agenda_ui.js
+++ bootstrap/js/agenda_ui.js
@@ -1,44 +1,49 @@
-// Toggle More or Less panel descriptions
-(function() {
-    $('.more-less').on("hide.bs.collapse", function() {
-        $(this).children().children('a[data-toggle]').text('More');
-    })
-    .on("show.bs.collapse", function() {
-        $(this).children().children('a[data-toggle]').text('Less');
-    });
-
-    // Hover effect turning opacity on
-    $('.bubble-md, .bubble-sm').mouseover(function() {
-        $(this).children().children('div').css('opacity', '0.5');
-    })
-    .mouseout(function() {
-        $(this).children().children('div').css('opacity', '1');
-    });
-})();
-
-// Display top button to return top
-(function() {
-    $(window).on('scroll', function() {
-        var appearHeight = $('.anchor').eq(1).offset().top;
-        if (window.pageYOffset >= appearHeight) {
-            $('.top-navigation').fadeIn();
-        
-        } else {
-            $('.top-navigation').fadeOut();
-        }
-    });
-
-    $('.top-navigation').click(function() {
-        window.scrollTo(0, 0);
-    });
-})();
-
+/*global $, Jquery, window, console, onscroll*/
 // Code to create the affix effect (complements of adussaq)
-/*global $, window*/
+// Please note: this code is designed to run after the html has loaded
 (function () {
     'use strict';
 
-    var setParameters, nestThem, resetParents, table$, originalHeader$;
+    var appearHeight, setParameters, nestThem, resetParents, table$,
+            originalHeader$, currentHash = window.location.hash,
+            getHashLocations, addHashToUrl, anchorLocations;
+
+    addHashToUrl = function (hash) {
+        //If we can manipulate this
+        if (history) {
+            if (hash.length === 0) {
+                //No hash passed, set it to nothing
+                history.pushState(
+                    '',
+                    document.title,
+                    window.location.pathname
+                );
+            } else {
+                //Hash passed, set it to the appropriate value
+                history.pushState(
+                    '',
+                    document.title,
+                    window.location.pathname + '#' + hash
+                );
+            }
+        }
+    };
+
+    getHashLocations = function () {
+        anchorLocations = [{
+            location: 0,
+            id: ''
+        }];
+        $('.anchor').each(function () {
+            anchorLocations.push({
+                location: $(this).offset().top,
+                id: $(this).attr('id')
+            });
+        });
+        anchorLocations = anchorLocations.sort(function (a, b) {
+            return a.location - b.location;
+        });
+    };
 
     setParameters = function (index) {
         var $agendaDay = $(this), navbarHeight = $('.navbar').height(),
@@ -86,6 +91,34 @@
         return $temp[0];
     };
 
+    //Set up the onscroll event
+    window.onscroll = function () {
+        var top = window.pageYOffset, i, found = false,
+                newHash = anchorLocations[anchorLocations.length - 1].id;
+
+        //First check to see if 'top button' should be there
+        if (top >= appearHeight) {
+            $('.top-navigation').fadeIn();
+        } else {
+            $('.top-navigation').fadeOut();
+        }
+
+        //Now check to see if the hash has changed
+        for (i = 0; !found && i < anchorLocations.length - 1; i += 1) {
+            if (top >= anchorLocations[i].location &&
+                    top < anchorLocations[i + 1].location) {
+                found = true;
+                newHash = anchorLocations[i].id;
+            }
+        }
+
+        if (newHash !== currentHash) {
+            addHashToUrl(newHash);
+			currentHash = newHash;
+        }
+    };
+
+    //Actually sets up all the elements
     $(window).load(function () {
         var header$, parent$;
         //Set the table array
@@ -101,6 +134,31 @@
         //Now set all the affix parameters
         header$.each(setParameters);
 
+        //Set up 'to top' button
+        $('.top-navigation').click(function () {
+            window.scrollTo(0, 0);
+        });
+        appearHeight = $('.anchor').eq(1).offset().top;
+
+        //Set up the url hash location object
+        getHashLocations();
+
+        // Toggle More or Less panel descriptions
+        $('.more-less').on("hide.bs.collapse", function () {
+            $(this).children().children('a[data-toggle]').text('More');
+            getHashLocations();
+        }).on("show.bs.collapse", function () {
+            $(this).children().children('a[data-toggle]').text('Less');
+            getHashLocations();
+        });
+
+        // Hover effect turning opacity on
+        $('.bubble-md, .bubble-sm').mouseover(function () {
+            $(this).children().children('div').css('opacity', '0.5');
+        }).mouseout(function () {
+            $(this).children().children('div').css('opacity', '1');
+        });
+
         $(window).on('resize', function () {
             //return the headers to the start point as far as HTML goes, this is
             // the only way to reset the affix points...
@@ -108,55 +166,12 @@
 
             //set the new parameters
             header$.each(setParameters);
+
+            //Reset height at which the 'top' button appears
+            appearHeight = $('.anchor').eq(1).offset().top;
+
+            //Set up the url hash location object
+            getHashLocations();
         });
     });
 }());
-
-// Code to add hashes where appropriate (compliments of adussaq)
-$(function () {
-    var addHashToUrl = function (hash) {
-        //If we can manipulate this
-        if (history) {
-            if (hash.length === 0) {
-                //No hash passed, set it to nothing
-                history.pushState(
-                    '',
-                    document.title,
-                    window.location.pathname
-                );
-            } else {
-                //Hash passed, set it to the appropriate value
-                history.pushState(
-                    '',
-                    document.title,
-                    window.location.pathname + '#' + hash
-                );
-            }
-        }
-    };
-
-    $(document).scroll(function () {
-        var i, top = window.pageYOffset, currentHash = window.location.hash,
-                //These last two values initialize the loop below
-                last_loc = -1, newHash = "", found = 0;
-
-        //Get each of the possibilities and see if that is where we are
-        $('.anchor').each(function (index) {
-            var this_loc = $(this).offset().top;
-            if (!found) {
-                if (top > last_loc && top < this_loc) {
-                    //We found it!
-                    found = 1;
-                } else {
-                    //If we are not there then change the values to be set
-                    last_loc = this_loc;
-                    newHash = $(this).attr('id');
-                }
-            }
-        });
-        //Now set the location if it has changed
-        if (newHash !== currentHash) {
-            addHashToUrl(newHash);
-        }
-    });
-});
